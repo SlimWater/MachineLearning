b07fbc03818337809604ecf867976254a46dd6c8
Xinying <xywang101@hotmail.com>
2018-01-08 16:10:05 -0600

    01082018-2nd

--- cnn/cnn.py
+++ cnn/cnn.py
@@ -28,11 +28,12 @@ class ConvLayer(object):
         self.padded_input_array = self.padding(input_array, self.zero_padding)
         for f in range(self.filter_number):
             filter = self.filters[f]
-            ConvLayer.conv(self.padded_input_array, filter.get_weights(), self.output_array[f], self.stride, filter.get_bias())
+            self.conv(self.padded_input_array, filter.get_weights(), self.output_array[f], self.stride, filter.get_bias())
             self.output_array[f] = self.elementOP(self.output_array[f], self.activator, "forward")
         #element_wist_op(self.output_array, self.activator.forward) activation function implementation
 
     def padding(self, input_array, zp):
+        zp = int(zp)
         if zp == 0:
             return input_array
         else:
@@ -40,14 +41,14 @@ class ConvLayer(object):
                 input_width = input_array.shape[2]
                 input_height = input_array.shape[1]
                 input_depth = input_array.shape[0]
-                padded_array = np.zeros((input_depth, input_height+2*zp, input_width+2*zp))
-                padded_array[:,zp:zp+input_height, zp:zp+input_width] = input_array
+                padded_array = np.zeros((input_depth, int(input_height+2*zp), int(input_width+2*zp)))
+                padded_array[:,zp:int(zp+input_height), zp:int(zp+input_width)] = input_array
 
             elif input_array.ndim == 2:
                 input_width = input_array.shape[1]
                 input_height = input_array.shape[0]
-                padded_array = np.zeros((input_height+2*zp, input_width + 2*zp))
-                padded_array[zp:zp+input_height, zp:zp+input_width] = input_array
+                padded_array = np.zeros((int(input_height+2*zp), int(input_width+2*zp)))
+                padded_array[zp:int(zp+input_height), zp:int(zp+input_width)] = input_array
             else:
                 padded_array = input_array
                 print("\r\n Input array dimenstion is not supported")
@@ -55,31 +56,22 @@ class ConvLayer(object):
 
     def conv(self, input_array, kernel_array, output_array, stride, bias):
         #the output_array is a 3d matrix in this module. in this function the input is output_array[f]
-        output_width = output_array.shape[2]
-        output_height = output_array.shape[1]
-        output_depth = output_array.shape[0]
+        output_width = output_array.shape[1]
+        output_height = output_array.shape[0]
         kernel_width = kernel_array.shape[-1]
         kernel_height = kernel_array.shape[-2]
-        if input_array.ndim == 2:
-            for i in range(output_height):
-                for j in range(output_width):
-                    #numpy array range array[a:b] means a->b-1
-                    temp_array = input_array[i*stride:i*stride+kernel_height, j*stride:j*stride+kernel_width]
-                    output_array[i,j] = np.multiply(temp_array, kernel_array).sum() + bias
-
-        elif input_array.ndim == 3:
-            for k in range(output_depth):
-                for i in range(output_height):
-                    for j in range(output_width):
-                        for m in range(input_array.shape[0]):
-                            temp_array_input = input_array[m, i*stride:i*stride+kernel_height, j*stride:j*stride+kernel_width]
-                            temp_array_filter = kernel_array[k,m,:,:]
-                            output_array[k, i, j] += np.multiply(temp_array_input, temp_array_filter).sum()
-                        output_array[k,i,j] += bias
+        for i in range(output_height):
+            for j in range(output_width):
+                output_array[i, j] = 0
+                for m in range(input_array.shape[0]):
+                    temp_array_input = input_array[m, int(i*stride):int(i*stride+kernel_height), int(j*stride):int(j*stride+kernel_width)]
+                    temp_array_filter = kernel_array[m]
+                    output_array[i, j] += np.multiply(temp_array_input, temp_array_filter).sum()
+                output_array[i,j] += bias
 
 
     #now implement the training functions
-    def bp_sensitivity_map(self, sensitivity_array, activator):
+    def backward(self, sensitivity_array, activator):
         #sensitivity_array: current layer sensitivity_map
         #upper layer activation function
         #to calculate upper layer sensitivity_map
@@ -91,11 +83,11 @@ class ConvLayer(object):
         #did not implement channels as I surpose that filter are transparent for all input channels
         for f in range(self.filter_number):
             filter = self.filters[f]
-            flipped_weights = np.rot90(filter.get_wieghts(),2)
+            flipped_weights = np.rot90(filter.get_weights(),2)
             delta_array = self.create_delta_array()
             for d in range(self.channel_number):
-                self.conv(expanded_array,flipped_weights[d],delta_array[d],stride=1,bias =0)
-            self.delta_array += delta_array
+                self.conv(expanded_array[d],flipped_weights[d],delta_array[d],stride=1,bias =0)
+                self.delta_array[f] += delta_array[d]
         derivative_array = self.elementOP(self.input_array, activator, "backward")
         self.delta_array *= derivative_array
 
@@ -116,8 +108,8 @@ class ConvLayer(object):
         expanded_width = self.input_width - self.filter_width + 2*self.zero_padding +1
         expanded_height = self.input_height - self.filter_height + 2 * self.zero_padding + 1
         expand_array =  np.zeros((depth, expanded_height,expanded_width))
-        for i in range(self.output_height):
-            for j in range(self.output_width):
+        for i in range(int(self.output_height)):
+            for j in range(int(self.output_width)):
                 i_pos = i*self.stride
                 j_pos = j*self.stride
                 expand_array[:,i_pos,j_pos] = sensitivity_array[:,i, j]
@@ -129,15 +121,23 @@ class ConvLayer(object):
     def elementOP(self, input_array, activator,direction):
 
         output_array = np.zeros((input_array.shape))
-        for i in range(input_array.shape[0]):
-            for j in range(input_array.shape[1]):
-                for m in range(input_array.shape[2]):
+        if input_array.ndim == 2:
+            for i in range(input_array.shape[0]):
+                for j in range(input_array.shape[1]):
                     if direction == "forward":
-                        output_array[i,j,m] = activator.forward(input_array[i,j,m])
+                        output_array[i,j] = activator.forward(input_array[i,j])
                     elif direction == "backward":
-                        output_array[i, j, m] = activator.forward(input_array[i, j, m])
-        return output_array
-
+                        output_array[i, j] = activator.backward(input_array[i, j])
+            return output_array
+        elif input_array.ndim == 3:
+            for i in range(input_array.shape[0]):
+                for j in range(input_array.shape[1]):
+                    for m in range(input_array.shape[2]):
+                        if direction == "forward":
+                            output_array[i,j,m] = activator.forward(input_array[i,j,m])
+                        elif direction == "backward":
+                            output_array[i, j,m] = activator.backward(input_array[i,j,m])
+            return output_array
 
 
 class Filter(object):
@@ -147,7 +147,7 @@ class Filter(object):
         self.weights_grad = np.zeros(self.weights.shape)
         self.bias_grad = 0
 
-    def get_wieghts(self):
+    def get_weights(self):
         return self.weights
     def get_bias(self):
         return self.bias
--- cnn/test.py
+++ cnn/test.py
@@ -29,7 +29,7 @@ def init_test():
           [0,0,0],
           [1,2,1]]])
     relu = cnn.ReluActivator()
-    cl = cnn.ConvLayer(5,5,3,3,3,2,1,1,relu,0.001)
+    cl = cnn.ConvLayer(5,5,3,3,3,2,1,2,relu,0.001)
     cl.filters[0].weights = np.array(
         [[[-1,1,0],
           [0,1,0],
@@ -54,6 +54,36 @@ def init_test():
 
     return a, b, cl
 def gradient_check():
-
     error_function = lambda o: o.sum()
-a, b, cl = init_test()
+    a, b, cl = init_test()
+    cl.forward(a)
+    sensitivity_array = np.ones(cl.output_array.shape, dtpye = np.float64)
+    cl.backward(a, sensitivity_array, cl.activator)
+
+    epsilon = 10e-4
+    for d in range(cl.filters[0].weights_grad.shape[0]):
+        for i in range(cl.filters[0].weights_grad.shape[1]):
+            for j in range(cl.filters[0].weights_grad.shape[2]):
+                cl.filters[0].weights[d,i,j] += epsilon
+                cl.forward(a)
+                err1 = error_function(cl.output_array)
+                cl.filters[0].weights[d,i,j] -= 2*epsilon
+                cl.forward(a)
+                err2 = error_function(cl.output_array)
+                expect_grad = (err1 - err2) / (2 * epsilon)
+                cl.filters[0].weights[d,i,j] += epsilon
+                print ('weights(%d,%d,%d): expected - actural %f - %f' % ( d, i, j, expect_grad, cl.filters[0].weights_grad[d,i,j]))
+
+def test():
+    a, b, cl = init_test()
+    cl.forward(a)
+    print (cl.output_array)
+
+def test_bp():
+    a, b, cl = init_test()
+    cl.backward(a, cl.activator)
+    cl.update()
+    print (cl.filters[0])
+    print (cl.filters[1])
+
+test_bp()
